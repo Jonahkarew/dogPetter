dddc864e1db616f591679f674808a00887346e4b
Jonah Karew <jonahkarew@gmail.com>
2020-08-19 20:25:24 -0400

    added bcrypt to hah user passwords

--- backend/models/model.js
+++ backend/models/model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -14,6 +15,38 @@ const userSchema = new mongoose.Schema({
         String
     ]
 })
+
+const saltRounds = parseInt(process.env.SALTROUNDS)
+
+userSchema.pre("save", function createPassword(next){
+    if (this.isNew || this.isModified("password")){
+        const document = this;
+        bcrypt.hash(this.password, saltRounds, (err, hashedPassword) => {
+            if(err){
+                next(err)
+            }
+            else{
+                document.password = hashedPassword
+                next();
+            }
+        })
+    }
+})
+
+userSchema.methods.isCorrectPassword = function isCorrectPassword(password){
+    const document = this;
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(password, document.password, function compareCallback(err, same){
+            if (err){
+                console.log(err)
+                reject(err)
+            }
+            else{
+                resolve(same)
+            }
+        })
+    })
+}
 
 const User = mongoose.model("User", userSchema);
 
